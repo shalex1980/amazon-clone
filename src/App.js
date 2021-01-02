b03e9b991b2b7ca66b82d32441255a01dc4a3bf7
Alex <alex@localhost.localdomain>
2021-01-02 11:49:40 +0200

    first initialization

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import './App.css';
+import './App.css'
+import Header from './Components/Header/Header'
 
 function App() {
 
   // App
   return (
     <div className="app">
+      <Header />
       <h1>Hello New course Amazon Clone  </h1>
     </div>
   );
